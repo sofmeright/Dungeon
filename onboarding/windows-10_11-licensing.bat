d90b539e714f470f925b0b5836c14236edf60491
kai <kai.hamil@gmail.com>
2025-02-26 10:04:23 +0000

    Update onboarding/windows-10_11-licensing.bat

--- onboarding/windows-10_11-licensing.bat
+++ onboarding/windows-10_11-licensing.bat
@@ -1,7 +1,7 @@
 echo Register the KMS Server.
 slmgr.vbs /skms 10.55.6.123:1688
 echo Register Windows KMS License Key
-slmgr.vbs /ipk XXXXX-XXXXX-XXXXX-XXXXX-XXXXX
+slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
 echo Activate Windows...
 Slmgr.vbs /ato
 echo Confirm Activation Status...
