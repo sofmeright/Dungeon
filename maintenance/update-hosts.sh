a4012183de25c3749fc8040b6730145a4fc93539
SoFMeRight <sofmeright@gmail.com>
2025-10-05 03:23:08 +0000

    Remove legacy directories to match current dungeon state

--- maintenance/update-hosts.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook -i inventory/complete.yaml maintenance/update-hosts-linux_clustered.yaml
-ansible-playbook -i inventory/complete.yaml maintenance/update-hosts-linux_unclustered.yaml
